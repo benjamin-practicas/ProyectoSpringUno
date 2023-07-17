6bebd96d93f9624a2cd2577090a380134999c7b4
benjamin-practicas <69986645+benjamin-practicas@users.noreply.github.com>
2023-07-16 20:39:50 -0600

    se actualiza desde remoto

--- src/es/pildoras/IoC/DirectorEmpleado.java
+++ src/es/pildoras/IoC/DirectorEmpleado.java
@@ -5,7 +5,7 @@ public class DirectorEmpleado implements Empleados {
 	@Override
 	public String getTareas() {
 		// TODO Auto-generated method stub
-		return "se regresa tareas  de director emple";
+		return "se regresa tareas  de director empleadinio";
 	}
 
 }
